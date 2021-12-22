c52819761887387d8dadcaec933ce4f319755387
acapunks <contact@acapunks.com>
2021-12-22 20:30:26 +0800

    update prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
+    '@vue/standard',
     '@vue/typescript/recommended'
   ],
+  plugins: ['only-warn'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': 0,
+    'vue/singleline-html-element-content-newline': 'off'
   }
 }
