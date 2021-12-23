40afca73874cd3ef0ecb9c37beda8dc2a8eb3b73
acapunks <contact@acapunks.com>
2021-12-23 20:58:40 +0800

    mint

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <navbar class="fixed top-0 right-0 left-0" />
-  <div style="margin-top: 72px" class="acala-gradient-bg-light min-h-screen">
+
+  <main style="margin-top: 72px" class="acala-gradient-bg-light min-h-screen">
     <router-view />
-  </div>
+  </main>
+
+  <!-- footer -->
+  <footer class="py-3 bg-white">
+    <img src="@/assets/images/powered_by.webp" class="h-3 inline-block mr-2">
+    <a target="_blank" href="https://acala.network" rel="noopenner norefererr"><img src="@/assets/images/acala.svg" class="h-8 inline-block"></a>
+  </footer>
 </template>
 
 <script lang="ts">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="home">
-    <mint />
-  </div>
+  <mint />
 </template>
 
 <script lang="ts">
