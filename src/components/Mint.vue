33e824e7211a756c742fc5b1b5aceeab418ca8a3
acapunks <contact@acapunks.com>
2021-12-23 21:11:31 +0800

    update style

--- src/components/Mint.vue
+++ src/components/Mint.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="h-screen flex justify-center items-center">
     <figure class="shadow-lg">
-      <img src="@/assets/images/punks.png" width="240" height="240">
-      <figcaption class="comment text-center bg-white pb-3 pt-4 rounded-bottom">
+      <img src="@/assets/images/punks.png" width="280" height="280">
+      <figcaption class="comment text-center bg-white py-6 rounded-bottom">
         <button class="py-2 px-12 border rounded-full border-acared text-acared hover:text-white hover:bg-acared transition-colors">Mint</button>
         <div class="mt-3" style="font-weight: 600">9487 Punks Remaining!</div>
       </figcaption>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
     <div class="self-stretch flex items-center justify-between">
       <!-- logo -->
       <div class="self-stretch flex items-center mr-4">
-        <a href="#" class="inline-flex items-center">
+        <a href="/" class="inline-flex items-center">
           <img src="@/assets/logo.png" width="48" height="48"><span>Acapunks</span>
         </a>
       </div>
@@ -19,16 +19,16 @@
       <!-- left items -->
       <div class="lg:self-stretch lg:flex mt-4 lg:mt-0 ">
         <ul class="text-left self-stretch lg:flex items-center space-y-2 lg:space-y-0 lg:space-x-3">
-          <li><a href="#">Mint</a></li>
-          <li><a href="#">View my Punks</a></li>
-          <li><a href="#">Market Place</a></li>
+          <li><a href="#" class="text-white text-opacity-70 hover:text-opacity-100">Mint</a></li>
+          <li><a href="#" class="text-white text-opacity-70 hover:text-opacity-100">View my Punks</a></li>
+          <li><a href="#" class="text-white text-opacity-70 hover:text-opacity-100">Market Place</a></li>
         </ul>
       </div>
       <!-- right items -->
       <div class="lg:self-stretch lg:flex mt-4 lg:mt-0 border-t lg:border-t-0 pt-4 lg:pt-0">
-        <ul class="text-left self-stretch justify-end lg:flex space-y-2 lg:space-y-0 items-center lg:space-x-3">
-          <li><a href="//twitter.com/acapunks" target="_blank" rel="noopener noreferrer"><fa :icon="['fab', 'twitter']" class="mr-1" />Twitter</a></li>
-          <li><a href="//discord.com/acapunks" target="_blank" rel="noopener noreferrer"><fa :icon="['fab', 'discord']" class="mr-1" />Discord</a></li>
+        <ul class="text-left self-stretch justify-end lg:flex space-y-2 lg:space-y-0 items-center lg:space-x-3 text-opacity-70 hover:text-opacity-100">
+          <li><a href="//twitter.com/acapunks" target="_blank" rel="noopener noreferrer" class="text-white text-opacity-70 hover:text-opacity-100"><fa :icon="['fab', 'twitter']" class="mr-1" />Twitter</a></li>
+          <li><a href="//discord.com/acapunks" target="_blank" rel="noopener noreferrer" class="text-white text-opacity-70 hover:text-opacity-100"><fa :icon="['fab', 'discord']" class="mr-1" />Discord</a></li>
         </ul>
         <div class="lg:ml-8">
           <button class="border rounded-full px-12 py-3 my-4 lg:my-0">Connect to Wallet</button>
