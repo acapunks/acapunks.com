e50a2b7ecba86a9e82cbdc85f742482663790f01
acapunks <contact@acapunks.com>
2022-01-03 08:12:38 +0800

    update style

--- src/components/Mint.vue
+++ src/components/Mint.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="h-screen-no-navbar flex justify-center items-center">
     <figure class="shadow-lg">
-      <img src="@/assets/images/mint.gif" width="280" height="280">
-      <figcaption class="text-center bg-white py-6 rounded-bottom">
-        <div class="mb-8 px-16">
-          <input v-model="mintCount" type="range" min="1" max="10" class="mint-bar mb-3 w-full"><br>
-          <button class="block w-full py-2 px-12 border rounded-full border-acared text-acared active:text-white hover:shadow active:bg-acared active-bg-opacity-1 transition-colors" @click="onMint">Mint <span class="inline-block text-center" style="width: 1em">{{ mintCount }}</span></button>
+      <img src="@/assets/images/mint.gif" width="280" height="280" class="shadow">
+      <figcaption class="bg-white rounded-bottom">
+        <div class="py-6 flex flex-col items-center bg-gray-50" style="box-shadow: inset 0px 4px 4px -4px #AAA, inset 0px -4px 4px -4px #AAA">
+          <input v-model="mintCount" type="range" min="1" max="10" class="mint-bar mb-3 w-full block" style="width: 80%">
+          <button class="block w-full py-2 border rounded-full border-acared text-acared active:text-white hover:shadow active:bg-acared active-bg-opacity-1 transition-colors" style="width: 60%" @click="onMint">Mint <span class="inline-block text-right" style="width: 1em">{{ mintCount }}</span></button>
         </div>
-        <div class="mt-3" style="font-weight: 600">9487 Punks Remaining!</div>
+        <div class="text-center py-6" style="font-weight: 600">9487 Punks Remaining!</div>
       </figcaption>
     </figure>
   </section>
