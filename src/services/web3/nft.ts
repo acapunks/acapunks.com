cc6da09a8643469d8b92057560c21f4a7e8de328
acapunks <contact@acapunks.com>
2022-01-19 07:01:58 +0800

    simple feature

--- src/services/web3/nft.ts
+++ src/services/web3/nft.ts
@@ -11,6 +11,11 @@ import { getUserProvider, getAnonymousProvider } from './wallet'
 const requiredConfirmCount = 3 // TODO: how many?
 const whitelistKey = new Uint8Array() // ethers.utils.arrayify('0x1234') will be changed when acala network is online
 
+export interface NftMeta {
+  tokenId: number
+  imageUrl: undefined | string
+}
+
 export async function mint(count: number): Promise<() => Promise<void>> {
   const _count = { value: ethers.utils.parseUnits(count.toString(), '16') }
   const ta: TransactionResponse = await acapunks
@@ -27,24 +32,43 @@ export async function mint(count: number): Promise<() => Promise<void>> {
   }
 }
 
-export async function getNftCount(addr: string): Promise<number> {
+export async function getOwnedNftCount(addr: string): Promise<number> {
   const ret: BigNumber = await acapunks.getReadonlyContract().balanceOf(addr)
   return ret.toNumber()
 }
 
-async function getNftUri(addr: string, index: number) {
-  const contract = acapunks.getReadonlyContract()
-  const tokenId: BigNumber = await contract.tokenOfOwnerByIndex(addr, index)
+async function getNftUrlByTokenId(tokenId: number): Promise<string> {
+  // a fake 50 ms delay for fetching url using index
+  await new Promise(res => setTimeout(res, 50))
 
-  // In test phase just return token id
-  return tokenId.toNumber()
-  
-  // In production return the metadata
-  const result: string = await contract.tokenURI(tokenId)
-  return result
+  // In test phase just return random image anyway
+  return 'https://picsum.photos/id/' + ((tokenId % 64) + 1) + '/64'
 }
 
-export function getNfts(addr: string, offset: number, count: number) {
-  const tasks = [...Array(count).keys()].map(i => getNftUri(addr, i + offset))
-  return Promise.all(tasks)
+async function getNftUrlByAddressIndex(
+  addr: string,
+  index: number,
+  dest: Array<null | NftMeta>
+): Promise<void> {
+  const contract = acapunks.getReadonlyContract()
+  const _tokenId: BigNumber = await contract.tokenOfOwnerByIndex(addr, index)
+  const tokenId = _tokenId.toNumber()
+  const imageUrl = await getNftUrlByTokenId(tokenId)
+  dest[index] = { tokenId, imageUrl }
+}
+
+export async function fetchNftMeta(
+  addr: string,
+  offset: number,
+  count: number,
+  dest: Array<null | NftMeta>
+): Promise<void> {
+  // Load 15 elements at once to prevent rate limits
+  const limit = 5
+  const delay = 1000
+  for (let i = offset; i < offset + count; i++) {
+    if (i - offset > 0 && (i - offset) % limit === 0)
+      await new Promise(res => setTimeout(res, delay))
+    getNftUrlByAddressIndex(addr, i + offset, dest) // fire and forget
+  }
 }
--- src/views/MyPunks.vue
+++ src/views/MyPunks.vue
@@ -1,35 +1,39 @@
 <template>
   <section id="my-punks" class="container mx-auto grow py-20 px-4 flex items-center">
-    <div>
-      <div class="md:flex items-stretch mb-4 pb-9 border-b border-slate-300">
-        <div class="grow flex items-center">
+    <div class="w-full">
+      <div class="md:flex items-stretch">
+        <div class="grow items-center" :class="nftCount && nftCount > 0 ? 'flex' : 'block'">
           <div>
-            <h1 class="text-2xl md:text-3xl lg:text-4xl mb-1">You have 87 Punks!</h1>
-            <p class="text-sm md:text-base"><a href="/#mint">Mint</a> more Punks.</p>
+            <h1 class="text-2xl md:text-3xl lg:text-4xl mb-1 text-center" :class="nftCount && nftCount > 0 ? 'text-left' : 'text-center'">
+              <span v-if="nftCount === null">Loading...</span>
+              <span v-else-if="nftCount === 0">You have no AcaPunks.</span>
+              <span v-else>You have {{ nftCount }} AcaPunks!</span>
+            </h1>
+            <p v-if="nftCount !== null" class="text-sm md:text-base" :class="nftCount && nftCount > 0 ? 'text-left' : 'text-center'">
+              <span v-if="nftCount === 0">
+                Click
+                <a href="/#mint">here</a> to Mint an AcaPunks!
+              </span>
+              <span v-else>
+                <a href="/#mint">Mint</a> more Punks.
+              </span>
+            </p>
           </div>
         </div>
-        <div class="hidden md:block aspect-square bg-slate-200" style="height: 25vh"></div>
+        <div v-if="nftCount && nftCount > 0" class="hidden md:block aspect-square bg-slate-200" style="height: 25vh">
+          <img :src="detailedImageUrl" class="w-full aspect-square" />
+        </div>
       </div>
-      <div>
-        <p class="hidden md:block text-right mb-4">Hover or touch the punk to show detailed.</p>
+      <div v-if="nftCount !== 0" class="pt-4 mt-9 border-t border-slate-300">
+        <p class="hidden md:block text-right mb-4">Click the punk to show the detail.</p>
         <div class="grid grid-cols-2 md:grid-cols-6 xl:grid-cols-10 gap-4">
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
-          <div class="punks-wrapper"><img src="/demo/1.svg" class="w-full" /><p class="text-right mt-1">#87</p></div>
+          <div v-for="nft in nftMeta" class="punks-wrapper">
+            <div class="w-full aspect-square relative">
+              <div class="skeleton w-full h-full absolute top-0 left-0"></div>
+              <img loading="lazy" :src="nft?.imageUrl || emptyImage" class="w-full h-full absolute top-0 left-0 cursor-pointer" @onload="onNftImageLoaded" @click="displayImage" />
+            </div>
+            <p class="text-right mt-1">#{{ nft?.tokenId || '' }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -37,13 +41,77 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { ref, reactive } from 'vue'
 import { useWalletStore } from '@/store/wallet'
-import { getNfts } from '@/services/web3/nft'
+import { getOwnedNftCount, fetchNftMeta, NftMeta } from '@/services/web3/nft'
 
 const wallet = useWalletStore()
-onMounted(async () => {
-  const poop = await getNfts(wallet.address as string, 0, 50)
-  console.log(poop)
-})
+const nftCount = ref(null as null | number)
+const nftMeta = reactive(Array<null | NftMeta>())
+const emptyImage = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
+const detailedImageUrl = ref(emptyImage)
+
+async function init() {
+  const address = wallet.address!
+  nftCount.value = await getOwnedNftCount(address)
+  for (let i = 0; i < nftCount.value; i++) {
+    nftMeta.push(null)
+  }
+  fetchNftMeta(wallet.address!, 0, nftCount.value, nftMeta)
+}
+
+function onNftImageLoaded(e: Event) {
+  // when the image is loaded remove the skeleton
+  const target = e.target as HTMLImageElement
+  const parent = target.parentNode as HTMLDivElement
+  const skeleton = parent.getElementsByClassName('skeleton')[0] as HTMLDivElement
+  skeleton.remove()
+}
+
+function displayImage(e: Event) {
+  console.log('???')
+  const target = e.target as HTMLImageElement
+  detailedImageUrl.value = target.src
+}
+
+init()
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/color";
+
+.skeleton {
+  background-repeat: no-repeat;
+  background-image: $aca-gradient-light;
+  position: relative;
+  overflow: hidden;
+
+  &:after {
+    display: block;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    transform: translateX(-100%);
+    // z-index: 1;
+
+    background: linear-gradient(
+      90deg,
+      transparent,
+      rgba(255, 255, 255, 0.2),
+      transparent
+    );
+
+    /* Adding animation */
+    animation: loading 1.5s infinite;
+  }
+}
+
+@keyframes loading {
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
